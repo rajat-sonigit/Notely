0058a5f372bb93fadf7c09bc8202be982b9350f4
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2024-12-17 10:26:29 +0530

    Update signup.dart

--- notely/lib/Screens/auth/signup.dart
+++ notely/lib/Screens/auth/signup.dart
@@ -19,7 +19,6 @@ class Signup extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              
               Container(
                 color: primaryColor,
                 height: 50.h,
