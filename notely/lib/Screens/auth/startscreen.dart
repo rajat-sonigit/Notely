f8be82484e7ef718461291f8cc2d196118406601
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2024-12-30 17:46:38 +0530

    Update startscreen.dart

--- notely/lib/Screens/auth/startscreen.dart
+++ notely/lib/Screens/auth/startscreen.dart
@@ -8,7 +8,6 @@ import 'package:sizer/sizer.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
