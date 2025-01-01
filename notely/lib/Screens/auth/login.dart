14ce1a57777108ef0fa88241b46e0d6cc5a6c556
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2025-01-01 10:41:18 +0530

    Update login.dart

--- notely/lib/Screens/auth/login.dart
+++ notely/lib/Screens/auth/login.dart
@@ -7,6 +7,7 @@ import 'package:sizer/sizer.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
