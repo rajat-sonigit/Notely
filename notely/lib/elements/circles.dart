0a193a207546451da74576f348dd63209d8581fc
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2025-01-02 15:08:58 +0530

    Update circles.dart

--- notely/lib/elements/circles.dart
+++ notely/lib/elements/circles.dart
@@ -6,7 +6,6 @@ class SmallCircle extends StatelessWidget {
 
   const SmallCircle({Key? key, required this.size, required this.color})
       : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Container(
