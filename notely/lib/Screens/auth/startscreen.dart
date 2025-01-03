6db90020e4d20a86753187c0889869c97e5341d4
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2025-01-03 09:54:16 +0530

    Update startscreen.dart

--- notely/lib/Screens/auth/startscreen.dart
+++ notely/lib/Screens/auth/startscreen.dart
@@ -118,6 +118,7 @@ class StartScreen extends StatelessWidget {
                               
                             ),
                           )
+                          
                         ],
                       )
                     ],
